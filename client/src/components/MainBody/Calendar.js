913e45fff6ecee0584a56716337961aa53c39a2a
PoganyBenedek <poganybenedek@gmail.com>
2023-08-18 20:42:22 +0200

    Openable tooltip on calendar

--- client/src/components/MainBody/Calendar.js
+++ client/src/components/MainBody/Calendar.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { format, startOfMonth, endOfMonth, eachDayOfInterval, isSameDay } from 'date-fns';
+import Overlay from 'react-bootstrap/Overlay';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 export default function Calendar() {
     const [currentDate, setCurrentDate] = useState(new Date());
     const events = [
-        { title: 'Event 1',color:'red', start: new Date(), end: new Date() },
-        // ... other events
+        { title: 'Event 1', color: 'red', start: new Date(), end: new Date() },
+        { title: 'Kutya séta', color: 'red', start: new Date(), end: new Date() },
+        { title: 'Kutya séta', color: 'red', start: new Date(), end: new Date() },
+        { title: 'Test', color: 'red', start: new Date(2023, 7, 9), end: new Date(2023, 8, 9) },
+        { title: 'Kutya séta', color: 'red', start: new Date(2023, 6, 9), end: new Date(2023, 8, 9) },
+        { title: '911', color: 'red', start: new Date(2023, 8, 11), end: new Date(2023, 8, 9) },
+        { title: 'test', color: 'red', start: new Date(2023, 8, 11), end: new Date(2023, 8, 9) },
+        { title: 'test2', color: 'red', start: new Date(2023, 8, 11), end: new Date(2023, 8, 9) },
+
     ];
+    const [show, setShow] = useState(false);
+    const [clickedDay, setClickedDay] = useState(null);
+    const target = useRef(null);
+
 
     const prevMonth = () => {
         setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1));
@@ -17,6 +30,7 @@ export default function Calendar() {
     };
 
     const daysInMonth = eachDayOfInterval({ start: startOfMonth(currentDate), end: endOfMonth(currentDate) });
+
 
     return (
         <div>
@@ -30,17 +44,37 @@ export default function Calendar() {
                 {daysInMonth.map((day) => (
                     <div key={day} className={`calendarDay ${isSameDay(day, new Date()) ? 'bg-dark text-white' : ''}`}>
                         <div >{format(day, 'd')}</div>
-                        <div >
-                            {events.map((event) => (
-                                isSameDay(event.start, day) && (
-                                    <div key={event.title} className={`event bg-${event.color}`}>
-                                        {event.title}
-                                    </div>
-                                )))}
+                        <div>
+                            {events.filter((event) => isSameDay(event.start, day)).length > 2 ?
+                                <div key={day} className="event" ref={target} onClick={() => { setShow(!show); setClickedDay(day) }}>
+                                    {events.filter((event) => isSameDay(event.start, day)).length} events
+                                </div>
+                                :
+                                events.map((event) => (
+                                    isSameDay(event.start, day) && (
+                                        <div key={event.title} className="event">
+                                            {event.title}
+                                        </div>
+                                    )
+                                ))
+                            }
                         </div>
                     </div>
                 ))}
             </div>
+            <Overlay target={target.current} show={show} placement="right">
+                {(props) => (
+                    <Tooltip id="overlay-example" {...props}>
+                        {events.map((event) => (
+                            isSameDay(event.start, clickedDay) && (
+                                <div key={event.title} className="event">
+                                    {event.title}
+                                </div>
+                            )
+                        ))}
+                    </Tooltip>
+                )}
+            </Overlay>
         </div>
     );
 }
--- client/src/components/MainBody/MainBody.js
+++ client/src/components/MainBody/MainBody.js
@@ -3,15 +3,15 @@ import Calendar from "./Calendar";
 export default function MainBody() {
     return (
         <div className="MainBody">
-                <ul class="nav flex-column nav-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#"><img src="/icons/calendarDark.svg" className="SideIcon" /></a>
+                <ul className="nav flex-column nav-tabs">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#"><img src="/icons/calendarDark.svg" className="SideIcon" /></a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><img src="/icons/cardCheckList.svg" className="SideIcon" /></a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#"><img src="/icons/cardCheckList.svg" className="SideIcon" /></a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><img src="/icons/group.svg" className="SideIcon" /></a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#"><img src="/icons/group.svg" className="SideIcon" /></a>
                     </li>
                 </ul>
                 
